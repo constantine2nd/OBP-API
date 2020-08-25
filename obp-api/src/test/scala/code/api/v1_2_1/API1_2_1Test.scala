f0fc8b8cc1718a3170982490844a47f2a6fd5e55
Marko Milić <marko@tesobe.com>
2020-08-25 10:58:29 +0200

    refactor/can_see_narrative => can_see_owner_comment

--- obp-api/src/test/scala/code/api/v1_2_1/API1_2_1Test.scala
+++ obp-api/src/test/scala/code/api/v1_2_1/API1_2_1Test.scala
@@ -50,7 +50,7 @@ class API1_2_1Test extends ServerSetupWithTestData with DefaultUsers with Privat
     "can_see_transaction_metadata","can_see_transaction_label","can_see_transaction_amount",
     "can_see_transaction_type","can_see_transaction_currency","can_see_transaction_start_date",
     "can_see_transaction_finish_date","can_see_transaction_balance","can_see_comments",
-    "can_see_narrative","can_see_tags","can_see_images","can_see_bank_account_owners",
+    "can_see_owner_comment","can_see_tags","can_see_images","can_see_bank_account_owners",
     "can_see_bank_account_type","can_see_bank_account_balance","can_see_bank_account_currency",
     "can_see_bank_account_label","can_see_bank_account_national_identifier",
     "can_see_bank_account_swift_bic","can_see_bank_account_iban","can_see_bank_account_number",
@@ -62,7 +62,7 @@ class API1_2_1Test extends ServerSetupWithTestData with DefaultUsers with Privat
     "can_see_physical_location","can_see_public_alias","can_see_private_alias","can_add_more_info",
     "can_add_url","can_add_image_url","can_add_open_corporates_url","can_add_corporate_location",
     "can_add_physical_location","can_add_public_alias","can_add_private_alias",
-    "can_delete_corporate_location","can_delete_physical_location","can_edit_narrative",
+    "can_delete_corporate_location","can_delete_physical_location","can_edit_owner_comment",
     "can_add_comment","can_delete_comment","can_add_tag","can_delete_tag","can_add_image",
     "can_delete_image","can_add_where_tag","can_see_where_tag","can_delete_where_tag"
   )
